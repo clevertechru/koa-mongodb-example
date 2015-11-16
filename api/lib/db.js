2bfc07cc8d9b89d4383a9b4378a9e74a78d6a24e
Elad Nava <eladnava@gmail.com>
2015-11-17 01:57:37 +0200

    Improve error handling
    Crash on startup if connection to MongoDB can’t be established.

--- api/lib/db.js
+++ api/lib/db.js
@@ -2,15 +2,7 @@
 var mongoose = require('mongoose');
 
 // Get config file for SQL connection
-var connection = mongoose.connect('localhost/test', function(err)
-{
-    // Handle connection errors
-    if (err)
-    {
-        // Log to console
-        console.log('Mongoose connection error: ' + err);
-    }
-});
+var connection = mongoose.connect('localhost/test');
 
 // Return connection
 module.exports = connection;
